7acd666978c6e2b776e9b2bad3d28c59fae66397
tonyimax <lin_hong_quan_msn@hotmail.com>
2025-05-08 09:45:31 +0800

    chang encode string to var

--- main.cpp
+++ main.cpp
@@ -3,16 +3,18 @@
 
 int main() {
     std::cout << "base64 test !\n";
-    wchar_t wc[128]={0};
+	constexpr char str[]="https://blog.csdn.net/fittec";
+	int len=128;
+    wchar_t wc[len]={0};
 	std::cout << "===test base64 encode===!\n";
-    CCustBase64::Encode("Hello",5,wc,128);
-	const size_t len = wcslen(wc) + 1;
+    CCustBase64::Encode(str,strlen(str),wc,len);
+	const size_t wlen = wcslen(wc) + 1;
 	char* out=CCustBase64::w2c(wc);
-	printf("===> 'Hello' encode to base64 is: '%s'\n\n",out);
+	printf("===> '%s' encode to base64 is: '%s'\n\n",str,out);
 	std::cout << "===test base64 encode===!\n";
 	printf("===> base64 String '%s' after base64 decode is:",out);
 
-	CCustBase64::Decode(wc,len,wc,128);
+	CCustBase64::Decode(wc,wlen,wc,len);
 	printf("'%s'\n\n",CCustBase64::w2c(wc));
 
     return 0;
